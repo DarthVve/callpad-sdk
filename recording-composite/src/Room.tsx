0077e243c206c1bcf75cf8637df045123f346e2b
Darth Vve <osheviremajoroh@gmail.com>
2026-01-15 01:24:43 +0100

    v1.9.4: feat:  recording composite performance updates

--- recording-composite/src/Room.tsx
+++ recording-composite/src/Room.tsx
@@ -1,10 +1,11 @@
 import {
   RoomAudioRenderer,
+  TrackReferenceOrPlaceholder,
   useRoomContext,
   useVisualStableUpdate,
 } from '@livekit/components-react';
 import { ConnectionState } from 'livekit-client';
-import { ReactElement } from 'react';
+import { ReactElement, useRef } from 'react';
 import useRecording from './useRecording';
 import { AudioOnlyLayout, VideoOnlyLayout } from './components';
 
@@ -12,27 +13,20 @@ export function CompositeTemplate() {
   const room = useRoomContext();
   const { remoteVideoTracks, isAudioOnly, hasScreenShare, screenshareTrack } = useRecording(room);
   const sortedTracks = useVisualStableUpdate(remoteVideoTracks, 1);
-  let mainTrack = sortedTracks.shift();
 
-  if (hasScreenShare) {
-    mainTrack = screenshareTrack
-  } else {
-    mainTrack = sortedTracks.shift();
-  }
+  const mainTrackRef = useRef<TrackReferenceOrPlaceholder | undefined>(undefined);
+  mainTrackRef.current = hasScreenShare ? screenshareTrack : sortedTracks[0];
+  const mainTrack = mainTrackRef.current;
   
   // determine layout to use
   let main: ReactElement = <></>;
   if (room.state !== ConnectionState.Disconnected) {
-    if (mainTrack) {
+    if (mainTrack && !isAudioOnly) {
       main = <VideoOnlyLayout mainTrack={mainTrack} remainingTracks={remoteVideoTracks!} />
     } else {
       main = (
         <AudioOnlyLayout tracks={remoteVideoTracks} />
       );
-    }
-
-    if (isAudioOnly) {
-      main = <AudioOnlyLayout tracks={remoteVideoTracks} />
     }
   }
 
